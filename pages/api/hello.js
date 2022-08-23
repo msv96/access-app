6dd0be5f86e42dc044e9b76c8039f74b8ae1906c
Srinivasan Mohan <srinivasan.m@leanfolks.com>
2022-08-23 14:28:29 +0530

    new method added

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { evaluate } from 'aatt';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  try {
+    const { url } = req.query;
+    const html = await fetch(url);
+    const doc = await html.text();
+    const data = await evaluate({
+      source: doc,
+      output: 'json',
+      engine: 'htmlcs',
+      level: 'WCAG2AAA',
+    });
+    const result = data?.split('Object]\n')[1];
+    res.status(200).json({ result: JSON.parse(result) });
+  } catch (error) {
+    res.status(500).json({ result: typeof error === 'string' ? error : JSON.stringify(error) });
+  }
 }
